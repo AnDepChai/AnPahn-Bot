f22f65b3cc1133f3dd78a5fc3c644058333fbd49
An Pahn <128393021+AnDepChai@users.noreply.github.com>
2025-11-06 21:29:37 +0700

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,1 +1,33 @@
+const { EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  data: {
+    name: 'avatar',
+    description: 'ʜɪểᴍ ᴛʜị ᴀᴠᴀᴛᴀʀ ᴄủᴀ ᴍộᴛ ɴɢườɪ ᴅùɴɢ.',
+    options: [
+      {
+        type: 6,
+        name: 'ai_đó',
+        description: 'ɴɢườɪ ᴅùɴɢ ʙạɴ ᴍᴜốɴ ʟấʏ ᴀᴠᴀᴛᴀʀ.',
+        required: false,
+      },
+    ],
+  },
+  async execute(interaction) {
+    const user = interaction.options.getUser('ai_đó') || interaction.user;
+    let format = user.displayAvatarURL().includes('.gif') ? 'gif' : 'png';
+    const avatarURL = user.displayAvatarURL({ format, size: 1024 });
+
+    const embed = new EmbedBuilder()
+      .setTitle(`ᴀᴠᴀᴛᴀʀ: ${user.tag}`)
+      .setDescription(`[ʟɪɴᴋ ᴀᴠᴀᴛᴀʀ:](${avatarURL})`)
+      .setImage(avatarURL)
+      .setColor('#2e3b46')
+      .setFooter({
+        text: `© ᴄᴏᴅᴇ ʙʏ ᴀɴ ᴘᴀʜɴ 🐧`
+      })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
